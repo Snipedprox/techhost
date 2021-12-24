c24fb8225c526f31deba2fd26e57b694aeda773c
Lexxy <85262576+Snipedprox@users.noreply.github.com>
2021-12-24 22:04:03 +0900

    Delete handler directory

--- handler/status.js
+++
@@ -1,7 +0,0 @@
-module.exports = (bot) => {
-    bot.status({
-        text: "In $servercount guilds and $allmemberscount members and my prefix is c.",
-        type: "PLAYING",
-        time: 12
-    })
-}
